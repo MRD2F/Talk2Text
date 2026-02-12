6e6729f7b00faa57b8087b1cedf90eb76df9ebeb
Enrico <enricobasciu@outlook.it>
2026-02-12 18:08:41 +0100

    project refactoring

--- src/app/main.py
+++ src/app/main.py
@@ -1,39 +1,13 @@
-from flask import Flask, render_template, request
-from app.convertor.service.convertor_service import ConvertorService
-
+from convertor.routes import upload_bp
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    # text = ConvertorService.create_text()
-    return render_template("main.html", person="enrico")
+    return "Talk2Text"
 
 
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return "No file part"
-
-    file = request.files["file"]
-
-    if file.filename == "":
-        return "No selected file"
-
-    # Instantiate service
-    convertor_service = ConvertorService(
-        model_id="tiny",
-        input_file_name=file.filename,  #  PATH to the saved file
-        show_text=True,
-        text_preview_size=100,
-    )
-
-    text = convertor_service.create_text()
-
-    return f"<h1>{text}</h1>"
-
-    # Leggi il contenuto del file in memoria
-    # data = file.read()  # bytes
-    # # oppure se è un testo: file_content = file.read().decode('utf-8')
-    # return f"Dimensione del file: {len(data)} byte"
+# Registrazione dei Blueprint
+app.register_blueprint(upload_bp)
