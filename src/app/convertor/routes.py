a423ae620d3308c3d7124d42907d122a51d0c81f
Enrico <enricobasciu@outlook.it>
2026-02-12 23:20:01 +0100

    fix validation

--- src/app/convertor/routes.py
+++ src/app/convertor/routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint
 from flask_restful import Api, Resource, request
 
+from app.convertor.service import FileService
+
 upload_bp = Blueprint("convertor", __name__, url_prefix="/convertor")
 api = Api(upload_bp)
 
 
 class FileUploadResource(Resource):
     def post(self):
-        file = request.files.get("file")
-
-        return {"message": "file conversion done", "data": "file"}, 200
+        file = FileService("mp3", 1000)
+        return file.convert(request.files.get("file"))
 
 
 api.add_resource(FileUploadResource, "/upload/")
--- src/app/convertor/service.py
+++ src/app/convertor/service.py
@@ -1,15 +1,12 @@
 class FileService:
-
     def __init__(self, allowed_extensions, max_size_mb):
         self.allowed_extensions = allowed_extensions
         self.max_size_mb = max_size_mb
 
     def validate(self, file):
         if not file:
-            return False
-
-
-        return True
+            return {"error": "Missing file"}, 400
 
     def convert(self, file):
-        return f"{file.filename} convertito"
+        self.validate(file)
+        return f"{file} converted"
